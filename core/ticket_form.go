d0b1cdd25c584580c88ace0347932fa3e81f6213
nukosuke <nukosuke@lavabit.com>
2018-05-28 17:24:09 +0900

    Add ticket_form API

---
+++ core/ticket_form.go
@@ -0,0 +1,47 @@
+package core
+
+import (
+	"encoding/json"
+	"github.com/nukosuke/go-zendesk/common"
+	"io/ioutil"
+	"net/http"
+)
+
+type TicketForm struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type GetTicketFormsResponse struct {
+	TicketForms []TicketForm `json:"ticket_forms"`
+	*common.Paginatable
+}
+
+func (s Service) GetTicketForms() (GetTicketFormsResponse, error) {
+	req, err := http.NewRequest("GET", s.BaseURL.String()+"/ticket_forms.json", nil)
+	if err != nil {
+		return GetTicketFormsResponse{}, err
+	}
+
+	req.Header.Set("User-Agent", s.UserAgent)
+	req.SetBasicAuth(s.Credential.Email(), s.Credential.Secret())
+
+	resp, err := s.HTTPClient.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return GetTicketFormsResponse{}, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return GetTicketFormsResponse{}, err
+	}
+
+	var ticketForms GetTicketFormsResponse
+	err = json.Unmarshal(body, &ticketForms)
+	if err != nil {
+		return GetTicketFormsResponse{}, err
+	}
+
+	return ticketForms, nil
+}
