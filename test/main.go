71a5dc945e504010c83da0fde50fb7d7f0c39c8a
nukosuke <nukosuke@lavabit.com>
2018-05-30 15:23:27 +0900

    fix

--- test/main.go
+++ test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/nukosuke/go-zendesk"
+	"github.com/nukosuke/go-zendesk/core"
 	"github.com/nukosuke/go-zendesk/common"
 	"net/http"
 	"os"
@@ -30,4 +31,23 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(triggers.Triggers)
+
+	ticketFields, page, err := client.Core.GetTicketFields()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(ticketFields)
+
+	ticketForms, err := client.Core.GetTicketForms()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("===== page has next")
+	fmt.Println(page.HasNext())
+	fmt.Println(ticketForms.TicketForms)
+
+	client.Core.PostTicketField(core.TicketField{Type: "text", Title: "Age"})
 }
