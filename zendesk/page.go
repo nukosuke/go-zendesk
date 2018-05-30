43e687c6a33dfd3173cc8ed46fe047ddd34bdf91
nukosuke <nukosuke@lavabit.com>
2018-05-30 16:32:02 +0900

    comment

--- zendesk/page.go
+++ zendesk/page.go
@@ -7,10 +7,12 @@ type Page struct {
 	Count        int64   `json:"count"`
 }
 
+// HasPrev checks if the Page has previous page
 func (p Page) HasPrev() bool {
 	return (p.NextPage != nil)
 }
 
+// HasNext checks if the Page has next page
 func (p Page) HasNext() bool {
 	return (p.NextPage != nil)
 }
