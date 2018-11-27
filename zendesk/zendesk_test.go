476206a94256805d38100cc124a7e5d5e879a337
nukosuke <nukosuke@lavabit.com>
2018-11-27 12:06:41 +0900

    Add more test

--- zendesk/zendesk_test.go
+++ zendesk/zendesk_test.go
@@ -5,6 +5,21 @@ import (
 	"testing"
 )
 
+func TestNewClient(t *testing.T) {
+	if _, err := NewClient(nil); err != nil {
+		t.Fatal("Failed to create Client")
+	}
+}
+
+func TestSetHeader(t *testing.T) {
+	client, _ := NewClient(nil)
+	client.SetHeader("Header1", "hogehoge")
+
+	if client.headers["Header1"] != "hogehoge" {
+		t.Fatal("Header1 is wrong")
+	}
+}
+
 func TestSetSubdomainSuccess(t *testing.T) {
 	validSubdomain := "subdomain"
 
