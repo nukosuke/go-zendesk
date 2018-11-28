eff8dff79d34c9c815d84507f59491d78fd05769
nukosuke <nukosuke@lavabit.com>
2018-11-29 00:33:47 +0900

    Add GetTriggers test

--- zendesk/trigger_test.go
+++ zendesk/trigger_test.go
@@ -1,1 +1,34 @@
 package zendesk
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// TODO: make API mocking helper
+func TestGetTriggers(t *testing.T) {
+	triggersJSON, err := ioutil.ReadFile("../test/fixtures/triggers.json")
+	if err != nil {
+		fmt.Printf("Failed to read fixture: %s", err)
+	}
+	mockAPI := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(triggersJSON)
+	}))
+	defer mockAPI.Close()
+
+	client, _ := NewClient(nil)
+	client.SetCredential(NewAPITokenCredential("", ""))
+	client.SetEndpointURL(mockAPI.URL)
+
+	triggers, _, err := client.GetTriggers()
+	if err != nil {
+		t.Fatalf("Failed to get triggers: %s", err)
+	}
+
+	if len(triggers) != 8 {
+		t.Fatalf("expected length of triggers is , but got %d", len(triggers))
+	}
+}
