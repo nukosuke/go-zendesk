3e03df11af5a1d29f91b9d4621e463159d5c7845
nukosuke <nukosuke@lavabit.com>
2018-12-01 01:39:47 +0900

    Add CreateGroup()

--- zendesk/group.go
+++ zendesk/group.go
@@ -54,3 +54,39 @@ func (z *Client) GetGroups() ([]Group, Page, error) {
 
 	return payload.Groups, payload.Page, nil
 }
+
+// CreateGroup creates new group
+// https://developer.zendesk.com/rest_api/docs/support/groups#create-group
+func (z *Client) CreateGroup(group Group) (Group, error) {
+	type Payload struct {
+		Group Group `json:"group"`
+	}
+
+	payload := Payload{Group: group}
+	req, err := z.NewPostRequest("/groups.json", payload)
+	if err != nil {
+		return Group{}, err
+	}
+
+	resp, err := z.httpClient.Do(req)
+	if err != nil {
+		return Group{}, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusCreated {
+		return Group{}, errors.New(http.StatusText(resp.StatusCode))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Group{}, err
+	}
+
+	var result Payload
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return Group{}, err
+	}
+
+	return result.Group, nil
+}
