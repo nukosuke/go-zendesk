18d781e8fdd5ff82b41aef95f12dd5183a7ee9e6
nukosuke <nukosuke@lavabit.com>
2018-12-03 12:16:34 +0900

    Add UserField definition

---
+++ zendesk/user_field.go
@@ -0,0 +1,25 @@
+package zendesk
+
+import (
+	"time"
+)
+
+// UserField is struct for user_field payload
+type UserField struct {
+	ID                  int64               `json:"id,omitempty"`
+	URL                 string              `json:"url,omitempty"`
+	Key                 string              `json:"key,omitempty"`
+	Type                string              `json:"type"`
+	Title               string              `json:"title"`
+	RawTitle            string              `json:"raw_title,omitempty"`
+	Description         string              `json:"description,omitempty"`
+	RawDescription      string              `json:"raw_description,omitempty"`
+	Position            int64               `json:"position,omitempty"`
+	Active              bool                `json:"active,omitempty"`
+	System              bool                `json:"system,omitempty"`
+	RegexpForValidation string              `json:"regexp_for_validation,omitempty"`
+	Tag                 string              `json:"tag,omitempty"`
+	CustomFieldOptions  []CustomFieldOption `json:"custom_field_options"`
+	CreatedAt           time.Time           `json:"created_at,omitempty"`
+	UpdatedAt           time.Time           `json:"updated_at,omitempty"`
+}
