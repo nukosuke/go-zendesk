8a6894c95b5569dc885a5284daab0800ee71ecd3
nukosuke <39.nuko@gmail.com>
2018-12-04 23:23:14 +0900

    Dynamic content support (#46)

---
+++ zendesk/locale.go
@@ -0,0 +1,36 @@
+package zendesk
+
+import (
+	"encoding/json"
+	"time"
+)
+
+// Locale is zendesk locale JSON payload format
+// https://developer.zendesk.com/rest_api/docs/support/locales
+type Locale struct {
+	ID        int64     `json:"id"`
+	URL       string    `json:"url"`
+	Locale    string    `json:"locale"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// GetLocales lists the translation locales available for the account.
+// https://developer.zendesk.com/rest_api/docs/support/locales#list-locales
+func (z *Client) GetLocales() ([]Locale, error) {
+	var data struct {
+		Locales []Locale `json:"locales"`
+	}
+
+	body, err := z.Get("/locales.json")
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data.Locales, nil
+}
---
+++ zendesk/locale_test.go
@@ -0,0 +1,21 @@
+package zendesk
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetLocales(t *testing.T) {
+	mockAPI := newMockAPI(http.MethodGet, "locales.json")
+	client := newTestClient(mockAPI)
+	defer mockAPI.Close()
+
+	locales, err := client.GetLocales()
+	if err != nil {
+		t.Fatalf("Failed to get locales: %s", err)
+	}
+
+	if len(locales) != 3 {
+		t.Fatalf("expected length of groups is 3, but got %d", len(locales))
+	}
+}
