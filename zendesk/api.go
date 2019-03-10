304fcc18dd0d7b0429ecc5cfe0657c6f6b781b40
Andy McCall <andymc903@gmail.com>
2019-03-10 11:31:41 -0400

    adding generated mock client

--- zendesk/api.go
+++ zendesk/api.go
@@ -1,5 +1,7 @@
 package zendesk
 
+//go:generate  mockgen -destination=mock/client.go -package=mock -mock_names=API=Client github.com/nukosuke/go-zendesk/zendesk API
+
 // API an interface containing all of the zendesk client methods
 type API interface {
 	GroupAPI
