d5f3bc70e30cc7a3e6bb13247aea4f416b3f7b63
nukosuke <nukosuke@lavabit.com>
2019-03-11 11:23:36 +0900

    Fix name of test func and case failure message

--- zendesk/ticket_form_test.go
+++ zendesk/ticket_form_test.go
@@ -20,13 +20,13 @@ func TestGetTicketForms(t *testing.T) {
 	}
 }
 
-func TestCreateTicketForms(t *testing.T) {
+func TestCreateTicketForm(t *testing.T) {
 	mockAPI := newMockAPIWithStatus(http.MethodPost, "ticket_form.json", http.StatusCreated)
 	client := newTestClient(mockAPI)
 	defer mockAPI.Close()
 
 	_, err := client.CreateTicketForm(TicketForm{})
 	if err != nil {
-		t.Fatalf("Failed to get ticket forms: %s", err)
+		t.Fatalf("Failed to send request to create ticket form: %s", err)
 	}
 }
