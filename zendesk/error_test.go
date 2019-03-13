8f7e80ce96202e609c95b0571d42f1869d82b6ab
Andy McCall <andymc903@gmail.com>
2019-03-13 09:27:19 -0400

    status code

--- zendesk/error_test.go
+++ zendesk/error_test.go
@@ -21,3 +21,39 @@ func TestError_Error(t *testing.T) {
 		t.Fatal("Error did not have expected value")
 	}
 }
+
+func TestError_Headers(t *testing.T) {
+	retryAfter := "Retry-After"
+	resp := &http.Response{
+		StatusCode: http.StatusTooManyRequests,
+		Header: http.Header{
+			retryAfter: []string{"92"},
+		},
+	}
+
+	err := Error{
+		resp: resp,
+	}
+
+	if _, ok := err.Headers()[retryAfter]; !ok {
+		t.Fatal("Could not get header values from zendesk error")
+	}
+}
+
+func TestError_Status(t *testing.T) {
+	retryAfter := "Retry-After"
+	resp := &http.Response{
+		StatusCode: http.StatusTooManyRequests,
+		Header: http.Header{
+			retryAfter: []string{"92"},
+		},
+	}
+
+	err := Error{
+		resp: resp,
+	}
+
+	if status := err.Status(); status != http.StatusTooManyRequests {
+		t.Fatal("Status returned from error was not the correct status code")
+	}
+}
