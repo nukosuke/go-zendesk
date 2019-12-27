d8cb311b390efb7d161ff8aa6eb9dd51bd5dc7da
Andy McCall <andymc903@gmail.com>
2019-12-26 23:04:27 -0800

    goimports

--- zendesk/sideload/ticket.go
+++ zendesk/sideload/ticket.go
@@ -3,12 +3,12 @@ package sideload
 import "time"
 
 type TicketDates struct {
-	AssigneeUpdatedAt *time.Time `json:"assignee_updated_at"`
-	RequesterUpdatedAt *time.Time `json:"requester_updated_at"`
-	StatusUpdatedAt *time.Time `json:"status_updated_at"`
-	InitiallyAssignedAt *time.Time `json:"initially_assigned_at"`
-	AssignedAt *time.Time `json:"assigned_at"`
-	SolvedAt *time.Time `json:"solved_at"`
+	AssigneeUpdatedAt    *time.Time `json:"assignee_updated_at"`
+	RequesterUpdatedAt   *time.Time `json:"requester_updated_at"`
+	StatusUpdatedAt      *time.Time `json:"status_updated_at"`
+	InitiallyAssignedAt  *time.Time `json:"initially_assigned_at"`
+	AssignedAt           *time.Time `json:"assigned_at"`
+	SolvedAt             *time.Time `json:"solved_at"`
 	LatestCommentAddedAt *time.Time `json:"latest_comment_added_at"`
 }
 
