ab5b016e3fefb74dfdabe4e761bbabb5a7835bf9
Luis Castañeda López <47322201+luiscastanedalopez@users.noreply.github.com>
2023-08-09 10:10:54 -0500

    Fixing issue with SearchUserOptions. The field name must be "external_id" not "external_ids"

--- zendesk/user.go
+++ zendesk/user.go
@@ -107,7 +107,7 @@ type UserRelated struct {
 // ref: https://developer.zendesk.com/api-reference/ticketing/users/users/#search-users
 type SearchUsersOptions struct {
 	PageOptions
-	ExternalIDs string `json:"external_ids,omitempty" url:"external_ids,omitempty"`
+	ExternalIDs string `json:"external_id,omitempty" url:"external_id,omitempty"`
 	Query       string `json:"query,omitempty" url:"query,omitempty"`
 }
 
